3a867cb696d822072fc9c39cbc0354ded8b6842a
nicolasDelessa <nicolas.lessa.ferreira@escola.pr.gov.br>
2026-06-12 09:00:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const flashcards = document.querySelectorAll('.flashcard');
+
+flashcards.forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flip');
+    });
+});
